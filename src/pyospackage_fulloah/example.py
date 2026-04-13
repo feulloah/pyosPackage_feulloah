d1e5b7901914fdc06096f7afa72fd9a40f525956
Felipe Ulloa <41088727+feulloah@users.noreply.github.com>
2026-04-13 17:47:05 +0000

    New function added to test the package. It multiplies number by summing

---
+++ src/pyospackage_fulloah/example.py
@@ -0,0 +1,70 @@
+"""
+A module that adds numbers together.
+
+You may want to delete this module or modify it for your package.
+It's generally good practice to have a docstring
+that explains the purpose of the module, at the top.
+"""
+
+def add_numbers(a, b):
+    """
+    Add two numbers together and return the result.
+
+    This is an example function with a numpy style docstring.
+    We recommend using this style for consistency and readability.
+
+    Parameters
+    ----------
+    a : float
+        The first number to add.
+    b : float
+        The second number to add.
+
+    Returns
+    -------
+    float
+        The sum of the two numbers.
+
+    Examples
+    --------
+    >>> add_numbers(3, 5)
+    8
+    >>> add_numbers(-2, 7)
+    5
+
+    """
+    return a + b
+
+
+def multiply_integers_slow(a,b):
+    """
+    Multiply 2 integers by counting.
+    Parameters
+    ----------
+    a : int
+        The first number to multiply.
+    b : int
+        The second number to multiply.
+
+    Returns
+    -------
+    int
+        The sum of the two numbers.
+
+    Examples
+    --------
+    >>> multiply_integers_slow(3, 5)
+    15
+    >>> multiply_integers_slow(-2, 7)
+    -14
+    """
+    a_sign = a>0
+    b_sign = b>0
+    if a_sign == b_sign:
+        c_sign = 1
+    else:
+        c_sign =-1
+    c = 0
+    for i in range(1, abs(b)+1):
+        c += abs(a)
+    return c_sign*c
